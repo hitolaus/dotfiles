955d5dba1b68a23d90ea78af91589783dd81fe68
Jakob Hilarius <jakob@syscall.dk>
2012-04-07 00:42:30 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Install the files in the home directory
+#
+# Jakob Hilarius <http://syscall.dk>, 2012
+#
+
+for f in $(ls -1a)
+do
+	# Ignore this and parent directory
+	if [[ "$f" == "." || "$f" == ".." ]]; then
+		continue;
+	fi
+	# Ignore this script
+	if [ "$f" == "$(basename $0)" ]; then
+		continue;
+	fi
+	if [ -e "$HOME/$f" ]; then
+		echo "$HOME/$f exist, ignoring"
+		continue;
+	fi
+	
+	ln -s "$(pwd)/$f" "$HOME/$f"
+done
